bf0f4f7f67514d0d6a85ec33acaac792f8269854
Merve Sıla Lif <mervsla@gmail.com>
2021-02-15 01:51:13 +0300

    Managers of Core layer added

---
+++ TelephoneDirectory.UserService.Core/Managers/ContactManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TelephoneDirectory.UserService.Core.DTO;
+using TelephoneDirectory.UserService.DataAccess.Models;
+using TelephoneDirectory.UserService.DataAccess.UOW;
+using TelephoneDirectory.UserService.DataAccess.USContext;
+
+namespace TelephoneDirectory.UserService.Core.Managers
+{
+  public class ContactManager
+    {
+        UnitOfWork uow = new UnitOfWork(new UserServiceContext());
+        public Contact ConvertToContact(UserContactDto contactDto)
+        {
+            Contact contact = new Contact();
+            contact.UserID = contactDto.UserID;
+            contact.PhoneNumber = contactDto.PhoneNumber;
+            contact.Email = contactDto.PhoneNumber;
+            contact.Address = contactDto.Address;
+
+            return contact;
+        }
+
+        public void AddContact(UserContactDto contactDto)
+        {
+            Contact contact = ConvertToContact(contactDto);
+            uow.ContactRepository.AddContact(contact);
+        }
+    }
+}
---
+++ TelephoneDirectory.UserService/Controllers/UserServiceController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelephoneDirectory.UserService.Core.DTO;
+using TelephoneDirectory.UserService.Core.Managers;
+using TelephoneDirectory.UserService.DataAccess.Models;
+using TelephoneDirectory.UserService.DataAccess.USContext;
+
+namespace TelephoneDirectory.UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserServiceController : Controller
+    {
+       
+        UserManager userManager = new UserManager();
+        ContactManager contactManager = new ContactManager();
+
+        [HttpPost]
+        public void AddPerson(UserContactDto userContactDto)
+        {
+            Guid guid = Guid.NewGuid();
+            userContactDto.UserID = guid;
+            User user = userManager.AddUser(userContactDto);
+            contactManager.AddContact(userContactDto);
+        }
+    }
+}
