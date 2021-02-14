9c1dba8e23231845b70b628a8de75d93fd9e5bcf
Merve Sıla Lif <mervsla@gmail.com>
2021-02-14 23:54:53 +0300

    UserService architecture created

---
+++ TelephoneDirectory.UserService.DataAccess/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelephoneDirectory.UserService.DataAccess.Models
+{
+    public class User
+    {
+        public User()
+        {
+            contacts = new List<Contact>();
+        }
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CompanyName { get; set; }
+
+        public ICollection<Contact> contacts { get; set; }
+    }
+}
